e5f9f1f685ecf94dc12d2d2cee82d3f1664e3f56
Victor Hugor <vichugor15@gmail.com>
2026-03-11 16:17:25 -0300

    Create att.js

---
+++ att.js
@@ -0,0 +1,6 @@
+let nomes = ['joão', 'pedro', 'maria', 'amanda']; 
+let numeros = [1,2,3,4,5,];
+    console.log(nomes);
+    nomes[2]='josé';
+    nomes.splice(2,0, 'kaka');
+    console.log(nomes);
